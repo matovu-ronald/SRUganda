bc2c4b62d8fb55e3d16fb51a267373e82187baf3
matovu-ronald <maronwhite6@gmail.com>
2018-09-23 12:05:40 +0300

    Modifying html markup to suit the new search criteria

--- resources/views/pages/check.blade.php
+++ resources/views/pages/check.blade.php
@@ -7,19 +7,53 @@
         	<div class="row">
         		<div class="col-sm-12 col-md-12 col-lg-12">
         		    <div class="msg">
-        		        <h3>Check Membership</h3>
-        		        <p>Enter your registration number and name to check your mebership:</p>
-        		        <div id="message-contact"></div>
+        		        
+						@if (session('results'))
+							@if(session()->get('results')->isEmpty())
+								<div class="bg-danger text-center" style="margin-bottom: 10px;">
+									<h3>Sorry, You have not registered yet, <a style="text-decoration: underline; color: #7080f5;" href="{{ url('membership') }}"> Click Here to Register</a> </h3>
+								</div>
+							@else 
+							<div class="text-center bg-success" style="margin-bottom: 10px;">
+								<h3>Please, Check Through the names returned to confirm your registration status</h3>
+							</div>
+							<div class="table-responsive">
+								<table class="table table-hover">
+									<thead>
+										<tr>
+											<th>Name</th>
+											<th>Registration Date</th>
+											<th>Registration Status</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach(session()->get('results') as $result)
+											<tr>
+												<td> {{ $result->name }} </td>
+												<td> {{ $result->created_at->toFormattedDateString() }} </td>
+												<td> {{ $result->confirmed == 1 ? 'Verified (Fully Registered) ' : 'Not Verified' }} </td>
+											</tr>
+										@endforeach
+									</tbody>
+								</table>
+							</div>
+							@endif
+						@endif
+						
+						
+						<div id="message-contact"></div>
+						<h3>Check Membership</h3>
+						<p>Enter your registration name to check your mebership:</p>
         		        <form action="{{ route('check') }}" enctype="multipart/form-data" method="post">
         		        	@csrf
         		        	<div class="row">
         		        		<div class="col-md-6">
     		        				<div class="form-group">
-    		        				    <input type="number" class="form-control" placeholder="Registration ID" required="required" id="visitor_email" name="id">
+    		        				    <input type="text" class="form-control" placeholder="Registration Name" required="required" id="visitor_email" name="name">
     		        				</div>
-    		        				@if ($errors->has('id'))
+    		        				@if ($errors->has('name'))
     		        				    <span class="invalid-feedback">
-    		        				        <strong>{{ $errors->first('id') }}</strong>
+    		        				        <strong>{{ $errors->first('name') }}</strong>
     		        				    </span>
     		        				@endif
         		        		</div>
